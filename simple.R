8ab9a7af8f5aff953c11300b0abc181d564cfd4c
anishmohan@gmail.com <anishmohan@gmail.com>
2015-08-29 09:30:30 -0700

    minor updates

--- simple.R
+++ simple.R
@@ -1,1 +1,17 @@
 x<-c(1,2,3)
+y=c(3,4,6)
+
+a=matrix(data=c(1,2,3,4,5,6), nrow=2, ncol=2)
+a
+
+#look at the matrix order
+b=matrix(data<-c(1,2,3,4,5,5))
+b
+
+c=matrix(data=c(1,2,3,4,5,6),2,3)
+c
+
+d=matrix(data=c(1,2,3,4,5,6),2,2, byrow=TRUE)
+d
+e=matrix(data=c(1,2,3,4,5,6),2,2, byrow=FALSE)
+e
